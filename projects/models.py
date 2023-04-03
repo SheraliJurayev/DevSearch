df53697273dc71a7df79d49535336a9670a5b453
SheraliJurayev <sheralijurayev0412@gmail.com>
2023-04-03 11:27:05 +0500

    Models

--- projects/models.py
+++ projects/models.py
@@ -9,6 +9,8 @@ class Project(models.Model):
     source_link = models.CharField(max_length=200 , null=True ,blank=True)
     tags = models.ManyToManyField('Tag', blank=True)
     vote_total = models.IntegerField(default=0 , null=True ,blank=True)
+    vote_ratio = models.IntegerField(default=0 , null=True ,blank=True)
+
     created = models.DateTimeField(auto_now_add=True)
     id = models.UUIDField(default=uuid.uuid4 , unique=True ,
                           primary_key=True , editable=False)
