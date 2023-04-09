621530a71fa3b3832fc53cf824200c8413b4fca0
SheraliJurayev <sheralijurayev0412@gmail.com>
2023-04-09 15:10:45 +0500

    CRUD

--- projects/urls.py
+++ projects/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import projects ,project , createProject
+from .views import projects ,project , createProject , updateProject
 
 urlpatterns = [ 
     path('' , projects , name='projects') , 
     path('project/<str:pk>/' , project , name='project') , 
-    path('create-project/', createProject , name='create-project') , 
+    path('create-project/', createProject , name='create-project') ,
+    path('update-project/<str:pk>/', updateProject , name='update-project') , 
 ]
--- projects/views.py
+++ projects/views.py
@@ -33,3 +33,19 @@ def createProject(request):
         "form" : form
         }
     return render(request, 'projects/project-form.html' , context)
+
+
+def updateProject(request , pk ):
+    project = Project.objects.get(id = pk)
+    form = ProjectForm(instance=project)
+
+    if request.method == 'POST':
+        form = ProjectForm(request.POST , instance=project)
+        if form.is_valid():
+            form.save()
+            return redirect('projects')
+
+    context = {
+        "form" : form
+        }
+    return render(request, 'projects/project-form.html' , context)
