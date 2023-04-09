12c626ae9ff9b53166435b139886ddf6e1878f1c
SheraliJurayev <sheralijurayev0412@gmail.com>
2023-04-09 16:02:45 +0500

    CRUD

--- projects/views.py
+++ projects/views.py
@@ -53,5 +53,8 @@ def updateProject(request , pk ):
 
 def deleteProject(request , pk):
     project = Project.objects.get(id=pk)
-    context = {'objects':project}
+    if request.method == 'POST':
+        project.delete()
+        return redirect('projects')
+    context = {'object':project}
     return render(request , 'projects/delete_template.html', context)
