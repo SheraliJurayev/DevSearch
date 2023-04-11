17f79e370310665a828f757e25c30db32adb3f5f
SheraliJurayev <sheralijurayev0412@gmail.com>
2023-04-11 14:50:20 +0500

    user-profile , and profiles , views

--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import profiles
+from .views import profiles , userProfile
 
 
 urlpatterns = [
-    path('' , profiles , name = 'profiles')
+    path('' , profiles , name = 'profiles') , 
+    path('profile/<str:pk>/' , userProfile , name = 'user-profile') , 
 ]
--- users/views.py
+++ users/views.py
@@ -7,3 +7,16 @@ def profiles(request):
         'profiles': profiles
     }
     return render(request , 'users/profiles.html' , context)
+
+def userProfile(request , pk):
+    profile = Profile.objects.get(id=pk)
+
+    topSkills = profile.skill_set.exclude(description__exact='')
+    otherSkills = profile.skill_set.filter(description = '')
+
+    context = {
+        "profile" : profile , 
+        'topSkills' : topSkills ,
+        'otherSkills' : otherSkills 
+    }
+    return render(request , 'users/user-profile.html' , context)
