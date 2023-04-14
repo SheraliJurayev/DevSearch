8ad838abf9e25bd1764e36879053d1ae1608f034
SheraliJurayev <sheralijurayev0412@gmail.com>
2023-04-15 01:55:08 +0500

    register forms

---
+++ users/forms.py
@@ -0,0 +1,11 @@
+from django.forms import ModelForm
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+
+class CustomUserCreationForm(UserCreationForm):
+    class Meta:
+        model = User
+        fields = ['first_name', 'email', 'username', 'password1' , 'password2']
+        labels = {
+            'first_name': 'Name',
+        }
--- users/views.py
+++ users/views.py
@@ -2,8 +2,8 @@ from django.shortcuts import render ,redirect
 from .models import Profile
 from django.contrib.auth import login , authenticate , logout
 from django.contrib import messages
-from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from .forms import CustomUserCreationForm
 
 
 def loginUser(request):
@@ -40,16 +40,21 @@ def logoutUser(request):
 
 def registerUser(request):
     page = 'register'
-    form = UserCreationForm()
+    form = CustomUserCreationForm()
 
     if request.method == 'POST':
-        form   = UserCreationForm(request.POST)
+        form   = CustomUserCreationForm(request.POST)
         if form.is_valid():
             user = form.save(commit = False)
             user.username = user.username.lower()
             user.save()
 
             messages.success( request , ' User was successfully registered' )
+
+            login(request , user)
+            return redirect('profiles')
+        else:
+            messages.success(request , 'An error has occured during registration') 
     context = {
         'page': page , 
         'form': form
