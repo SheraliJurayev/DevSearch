42d5832e125d39700eb05ff9d84895bb8d9cee67
SheraliJurayev <sheralijurayev0412@gmail.com>
2023-04-24 23:35:57 +0500

    dsa

--- projects/models.py
+++ projects/models.py
@@ -2,7 +2,6 @@ from django.db import models
 import uuid
 from users.models import Profile
 
-# Create your models here.
 class Project(models.Model):
     owner = models.ForeignKey(Profile , null = True , blank=True , on_delete=models.SET_NULL)
     title = models.CharField(max_length=200)
--- projects/urls.py
+++ projects/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import projects ,project , createProject , updateProject , deleteProject 
 
 urlpatterns = [ 
-    path('' , projects , name='projects') , 
+    path('' , projects , name='projects')  , 
     path('project/<str:pk>/' , project , name='project') , 
     path('create-project/', createProject , name='create-project') ,
     path('update-project/<str:pk>/', updateProject , name='update-project') , 
